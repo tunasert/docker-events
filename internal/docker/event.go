c1e2ff9840f16a6256cb58cf603e4637349dcb6c
Filippo Finke <filippofinke@protonmail.ch>
2025-09-30 16:08:23 +0200

    feat(docker): add Event model and conversion from docker events.Message

---
+++ internal/docker/event.go
@@ -0,0 +1,44 @@
+package docker
+
+import (
+	"time"
+
+	"github.com/docker/docker/api/types/events"
+)
+
+type Event struct {
+	ID        string
+	Status    string
+	From      string
+	Type      string
+	Action    string
+	Scope     string
+	Actor     Actor
+	Timestamp time.Time
+}
+
+type Actor struct {
+	ID         string
+	Attributes map[string]string
+}
+
+func convertMessage(msg events.Message) Event {
+	timestamp := time.Unix(0, msg.TimeNano)
+	if msg.TimeNano == 0 && msg.Time != 0 {
+		timestamp = time.Unix(msg.Time, 0)
+	}
+
+	return Event{
+		ID:     msg.ID,
+		Status: string(msg.Status),
+		From:   msg.From,
+		Type:   string(msg.Type),
+		Action: string(msg.Action),
+		Scope:  string(msg.Scope),
+		Actor: Actor{
+			ID:         msg.Actor.ID,
+			Attributes: msg.Actor.Attributes,
+		},
+		Timestamp: timestamp,
+	}
+}
